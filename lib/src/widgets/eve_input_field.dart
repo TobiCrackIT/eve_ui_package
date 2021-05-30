bac92cbf3cee1a5fcc686c0df41dfb8521770c08
oluwatobig <oluwatoyinoluwatobig@gmail.com>
2021-05-30 14:22:32 +0100

    extend input field

--- lib/src/widgets/eve_input_field.dart
+++ lib/src/widgets/eve_input_field.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 
 class EveInputField extends StatelessWidget {
   final TextEditingController controller;
+  final String? title;
   final String placeholder;
   final Widget? leading;
   final Widget? trailing;
+  final bool enabled;
   final bool password;
   final void Function()? trailingTapped;
 
@@ -16,46 +18,62 @@ class EveInputField extends StatelessWidget {
   EveInputField({
     Key? key,
     required this.controller,
+    this.title='',
     this.placeholder = '',
     this.leading,
     this.trailing,
     this.trailingTapped,
+    this.enabled=false,
     this.password = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextField(
-        controller: controller,
-        obscureText: password,
-        decoration: InputDecoration(
-          hintText: placeholder,
-          filled: true,
-          fillColor: kEveAliceBlue,
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-          prefixIcon: leading,
-          suffixIcon: trailing != null
-              ? GestureDetector(
-                  onTap: trailingTapped,
-                  child: trailing,
-                )
-              : null,
-          border: eveBorder.copyWith(
-            borderSide: BorderSide(color: kEveAliceBlue),
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title!,
+            style: Theme.of(context).textTheme.headline2!.copyWith(
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+              color: enabled
+                  ? kEveAliceBlue
+                  : kEveJacarta,
+            ),
           ),
-          disabledBorder: eveBorder.copyWith(
-            borderSide: BorderSide(color: kEveAliceBlue),
+          TextField(
+            controller: controller,
+            obscureText: password,
+            obscuringCharacter: '●',
+            decoration: InputDecoration(
+              hintText: placeholder,
+              filled: true,
+              fillColor: kEveAliceBlue,
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              prefixIcon: leading,
+              suffixIcon: trailing != null
+                  ? GestureDetector(
+                      onTap: trailingTapped,
+                      child: trailing,
+                    )
+                  : null,
+              border: eveBorder.copyWith(
+                borderSide: BorderSide(color: kEveAliceBlue),
+              ),
+              disabledBorder: eveBorder.copyWith(
+                borderSide: BorderSide(color: kEveAliceBlue),
+              ),
+              enabledBorder: eveBorder.copyWith(
+                borderSide: BorderSide(color: kEveAliceBlue),
+              ),
+              focusedBorder: eveBorder.copyWith(
+                borderSide: BorderSide(color: kEveCeruleanBlue),
+              ),
+            ),
           ),
-          enabledBorder: eveBorder.copyWith(
-            borderSide: BorderSide(color: kEveAliceBlue),
-          ),
-          focusedBorder: eveBorder.copyWith(
-            borderSide: BorderSide(color: kEveCeruleanBlue),
-          ),
-        ),
-      ),
-    );
+        ],
+      );
   }
 }
