ca1dfa0e37f2a60eedc964e76bc2fafdd14af795
oluwatobig <oluwatoyinoluwatobig@gmail.com>
2021-06-07 10:53:34 +0100

    add more colors

--- lib/src/shared/app_colors.dart
+++ lib/src/shared/app_colors.dart
@@ -6,10 +6,12 @@ const Color kEveBlue=Color(0xff0000FF);
 const Color kEveBorder=Color.fromRGBO(148, 142, 165, 0.15);
 const Color kEveCeruleanBlue = Color(0xff1949D8);
 const Color kEveDimGrey = Color(0xff786F6F);
+const Color kEveError=Color(0xFFA13226);
 const Color kEveGreen=Color(0xff00FF00);
 const Color kEveJacarta = Color(0xff3F3356);
 const Color kEveRed=Color(0xffFF0000);
 const Color kEveSolitude = Color(0xffE8EDFB);
+const Color kEveSuccess = Color(0xFF43BF75);
 const Color kEveViolentViolet = Color(0xff291D4A);
 const Color kEveWhite=Color(0xffFFFFFF);
 
